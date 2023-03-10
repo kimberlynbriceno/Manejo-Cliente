6170664ac74c0b56640b7309752205ecf3b2771d
kimberlyn <kimberlynbriceno22@gmail.com>
2023-03-10 17:39:37 -0500

    style-form

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation  } from "react-router-dom"
 
 const Layout = () => {
+   const location = useLocation()
+
+   console.log(location)
+
   return (
     <div className="md:flex md:min-h-screen">
-        <aside className="md:w-1/4 bg-purple-500 px-5 py-10"> 
-              crm 
-        </aside>
+        <aside className="md:w-1/4 bg-purple-500 px-5 py-10 "> 
+        
+              <h2 className="font-bold text-3xl text-white text-center"> CRM-Clientes</h2>
+              
+              <nav className="m-3 font-bold">
+                <Link className={`${location.pathname === '/' ? "text-purple-900" : "text-black"}  hover:text-white `} to="/">Clientes</Link> <br />
+                <Link className={`${location.pathname === '/Nuevo/Cliente' ? "text-purple-900" : "text-black"}  hover:text-white `}  to="/Nuevo/Cliente">Nuevo Cliente</Link>
+              </nav>
+          </aside>
         <main className="md:w-3/4 md:h-screen overflow-scroll ">
 
   <Outlet/>      
@@ -16,3 +26,4 @@ const Layout = () => {
 }
 
 export default Layout
+ 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,88 @@
+import { useLoaderData } from "react-router-dom";
+
+export function loader(){
+
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+return (clientes)
+}
+
 
 const Index = () => {
+  const clientes = useLoaderData();
+  console.log(clientes)
+
+
   return (
-    <div>
-      <h2>indice</h2>
-    </div>
+    <>
+    <h2 className="font-bold text-4xl text-purple-900 m-5">Clientes</h2>
+    <p className="font-bold m-5">Administrar Clientes</p>
+
+    {clientes.length ? (
+      <table className="w-full bg-white table-auto m-3">
+        <thead className="bg-purple-500 text-white">
+          <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.map( cliente => (
+            <tr className="p-5 ">
+              <td>
+                {cliente.nombre}
+              </td>
+            </tr>
+          ))}
+            <tr>
+
+            </tr>
+        </tbody>
+      </table>
+     ) : (
+
+      <p className='mt-5 text-center'> no hay clientes aun</p>
+    )}
+
+
+    </>
+
   )
 }
 
